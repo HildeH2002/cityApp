077a6e4aac99f241ee6916ef2f77eee844c2ad08
Hilde Hemelaers <r0839760@student.thomasmore.be>
2021-12-17 11:19:24 +0100

    Add project files.

---
+++ cityApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace cityApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCityResult = new System.Windows.Forms.RichTextBox();
+            this.btnCityResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCityResult
+            // 
+            this.txtCityResult.Location = new System.Drawing.Point(-1, -1);
+            this.txtCityResult.Name = "txtCityResult";
+            this.txtCityResult.Size = new System.Drawing.Size(416, 452);
+            this.txtCityResult.TabIndex = 0;
+            this.txtCityResult.Text = "";
+            // 
+            // btnCityResult
+            // 
+            this.btnCityResult.Location = new System.Drawing.Point(495, 396);
+            this.btnCityResult.Name = "btnCityResult";
+            this.btnCityResult.Size = new System.Drawing.Size(94, 29);
+            this.btnCityResult.TabIndex = 1;
+            this.btnCityResult.Text = "Show cities";
+            this.btnCityResult.UseVisualStyleBackColor = true;
+            this.btnCityResult.Click += new System.EventHandler(this.btnCityResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCityResult);
+            this.Controls.Add(this.txtCityResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtCityResult;
+        private System.Windows.Forms.Button btnCityResult;
+    }
+}
+
---
+++ cityApp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cityApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCityResult_Click(object sender, EventArgs e)
+        {
+            txtCityResult.Clear();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation={1}", 1000),
+                Headers =
+                {
+                    { "x-rapidapi-key", "29c2761eeemshafc9d5510c8cf46p1d0242jsna294253be3eb" },
+                    { "x-rapidapi-host", "wft-geo-db.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+
+
+                txtCityResult.Text = (body);
+
+            }
+        }
+    }
+}
